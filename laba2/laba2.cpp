9af0247f6b894df9e9068196ac39b8863ab7f4d2
Dmitriy <72145339+Dmitriy-Brashavets@users.noreply.github.com>
2020-09-30 23:20:46 +0300

    Update laba2.cpp

--- laba2/laba2.cpp
+++ laba2/laba2.cpp
@@ -1,36 +1,37 @@
-#include "stdafx.h" 
 #include <iostream> 
 using namespace std;
 
 
 void findNumber(char str[]) {
-	int size = 0;
-	while (str[size] != '\0')
-		++size;
-	for (int i = 0; i < size; i++)
-		if (str[i] >= '0' && str[i] <= '9') {
-			cout << str << endl;
-			break;
+	int startWord = 0;
+	int endWord = 0;
+	bool number = 0;
+	for (int i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= '0' && str[i] <= '9')
+			number++;
+		if (str[i] == ' ')
+		{
+			endWord = i;
+			if (number == 1)
+			{
+				for (int j = startWord; j < endWord; j++)
+					cout << str[j];
+				cout << endl;
+			}
+			startWord = i + 1;
+			number = 0;
 		}
+	}
 }
-int _tmain(int argc, _TCHAR* argv[])
+
+int main()
 {
 	setlocale(LC_ALL, "");
 	char str[256];
-	char words[128][128];
-	cout << "Введите строку: ";
+	cout << "ведите строку: ";
 	cin.getline(str, 255);
-	char * buf, *token;
-	int i = 0;
-	buf = strtok_s(str, " ", &token);
-	while (buf != NULL)
-	{
-		strcpy_s(words[i], buf);
-		++i;
-		buf = strtok_s(NULL, " ", &token);
-	}
-	for (int k = 0; k < i; k++)
-		findNumber(words[k]);
+	findNumber(str);
 	system("pause");
 	return 0;
 }
